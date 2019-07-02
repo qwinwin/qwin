e4ae6ffa9053ec6ace5aa2e0d12e3395df77316b
qwinwin7 <qwinwin7@gmail.com>
2019-07-02 16:37:21 +0800

    update docker-ce for ubuntu

--- qwin.sh
+++ qwin.sh
@@ -125,7 +125,22 @@ Install_Docker() {
     Check_OS
     case "$release" in
     ubuntu)
-        apt update && apt install -y docker.io && echo "Install Dokcer Successfully!"
+        apt-get remove docker docker-engine docker.io containerd runc
+        apt-get update
+        apt-get -y install \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        gnupg-agent \
+        software-properties-common
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+        add-apt-repository \
+        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) \
+        stable"
+        apt-get update
+        apt-get -y install docker-ce docker-ce-cli containerd.io && echo "Install Dokcer Successfully!"
+        systemctl start docker
         systemctl enable docker
         ;;
     debian)
