235e8dd93d679770d0f012e96d5aef199ca8c669
qwinwin <qwinwin7@gmail.com>
2020-12-10 15:17:09 +0800

    update

--- qwin.sh
+++ qwin.sh
@@ -224,9 +224,7 @@ EOF
 
 Install_Nginx() {
     wget -O /etc/apt/trusted.gpg.d/nginx-mainline.gpg https://packages.sury.org/nginx-mainline/apt.gpg
-    cat >>/etc/apt/sources.list.d/nginx.list <<EOF
-deb https://packages.sury.org/nginx-mainline/ $(lsb_release -sc) main
-EOF
+    sh -c 'echo "deb https://packages.sury.org/nginx-mainline/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/nginx-mainline.list'
     cat >>/etc/apt/preferences <<EOF
 Package: nginx*
 Pin: release a=buster-backports
